56ba6f10274cf4e4f2237dc3da13e2e4ac73919a
Bytes Crafter <bytescrafter@gmail.com>
2020-04-20 18:30:21 +0800

    Added the Unity Tanks assets with script modification.

---
+++ Assets/Demoguy/TankLogics.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Complete;
+
+public class TankLogics : MonoBehaviour
+{
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0)) {
+            CameraControl.cameraControl.focusList.Add(transform);
+        }
+    }
+
+    void OnDestroy() {
+         CameraControl.cameraControl.focusList.Remove(transform);
+    }
+}
